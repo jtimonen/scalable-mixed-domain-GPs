b898920093aad5b5fd5a580c11fc7a0d0344d174
jtimonen <juho.timonen@iki.fi>
2021-06-16 15:30:40 +0300

    smalll edit

--- example2.R
+++ example2.R
@@ -12,7 +12,7 @@ for (f in dir("R")) {
 
 # Simulate data
 sd <- simulate_data(
-  N = 6, t_data = seq(1, 5, by = 0.1),
+  N = 6, t_data = seq(1, 5, by = 0.05),
   relevances = c(0, 1, 1),
   covariates = c(2),
   lengthscales = c(1, 0.5, 0.5), t_jitter = 0.5
@@ -30,15 +30,15 @@ N <- stan_data$num_obs
 cat("num_obs =", N, "\n")
 
 # Sample model 1
-m1 <- stan_model("stan/lgp_covariance.stan")
-f1 <- sampling(m1, stan_data, chains = CHAINS)
+#m1 <- stan_model("stan/lgp_covariance.stan")
+#f1 <- sampling(m1, stan_data, chains = CHAINS)
 
 # Sample model 2
 m2 <- stan_model("stan/lgp_basisfun.stan")
 f2 <- sampling(m2, stan_data, chains = CHAINS)
 
 # Comparison plot
-ag_name <- paste0("M=", M_bf, ", L=", L_bf)
-plt <- plot_params_comparison(f1, f2, ag_name, N)
+#ag_name <- paste0("M=", M_bf, ", L=", L_bf)
+#plt <- plot_params_comparison(f1, f2, ag_name, N)
 
-ggsave("compare.pdf", plot=plt, width=9, height=5)
+#ggsave("compare.pdf", plot=plt, width=9, height=5)
