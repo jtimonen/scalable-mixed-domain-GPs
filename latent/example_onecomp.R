d92675615852bf7e949282ee3b500d7b71b1e98f
jtimonen <juho.timonen@iki.fi>
2021-08-26 01:01:07 +0300

    threecomp slow

--- latent/example_onecomp.R
+++ latent/example_onecomp.R
@@ -35,7 +35,7 @@ for (f in dir("R")) {
 # Create additional Stan input
 max_x <- max(abs(m1@stan_input$x_cont))
 num_bf <- 30
-scale_bf <- 1.25*max_x
+scale_bf <- 1.25 * max_x
 si_add <- setup_approx(m2, num_bf = num_bf, scale_bf = scale_bf)
 stan_data <- c(m1@stan_input, si_add)
 
@@ -44,13 +44,13 @@ sm1 <- stan_model("stan/lgp_latent_basisfun_onecomp.stan")
 f1 <- sampling(sm1, data = stan_data, cores = 4)
 
 # Create model and sample
-#sm2 <- stan_model("stan/lgp_latent_covariance.stan")
-#f2 <- sampling(sm2, data = stan_data, cores = 4)
+# sm2 <- stan_model("stan/lgp_latent_covariance.stan")
+# f2 <- sampling(sm2, data = stan_data, cores = 4)
 
 # Compare
 N <- stan_data$num_obs
-cat("N=",N,"\n",sep="")
-#plt <- plot_params_comparison_onecomp(f2, f1, ag_name = "approx", N = N)
+cat("N=", N, "\n", sep = "")
+# plt <- plot_params_comparison_onecomp(f2, f1, ag_name = "approx", N = N)
 
 # Compare functions
 f_draws1 <- extract(f1, pars = "f_latent")$f_latent
@@ -59,20 +59,21 @@ df1 <- data.frame(t(fl_1))
 colnames(df1) <- c("f1")
 df1 <- cbind(dat, df1)
 
-#f_draws2 <- extract(f2, pars = "f_latent")$f_latent
-#fl_2 <- apply(f_draws2, c(2, 3), mean)
-#df2 <- data.frame(t(fl_2))
-#colnames(df2) <- c("f1")
-#df2 <- cbind(dat, df2)
+# f_draws2 <- extract(f2, pars = "f_latent")$f_latent
+# fl_2 <- apply(f_draws2, c(2, 3), mean)
+# df2 <- data.frame(t(fl_2))
+# colnames(df2) <- c("f1")
+# df2 <- cbind(dat, df2)
 
-#type <- as.factor(rep(c("approx", "exact"), each = N))
-#df <- rbind(df1, df2)
-#df$type <- type
+# type <- as.factor(rep(c("approx", "exact"), each = N))
+# df <- rbind(df1, df2)
+# df$type <- type
 
-#p1 <- ggplot(df, aes(x = age, y = f1, group = type, color = type)) +
+# p1 <- ggplot(df, aes(x = age, y = f1, group = type, color = type)) +
 #  geom_line()
 # p2 <-  ggplot(df, aes(x=age,y=f2,group=z,color=type)) +geom_line()
 # p3 <-  ggplot(df, aes(x=age,y=f3,group=id,color=type)) +geom_line()
 
-p1 <- ggplot(df1, aes(x = age, y = f1)) + geom_line(color="firebrick") + 
-  geom_point(data=dat, mapping=aes(x=age, y=y))
+p1 <- ggplot(df1, aes(x = age, y = f1)) +
+  geom_line(color = "firebrick") +
+  geom_point(data = dat, mapping = aes(x = age, y = y))
