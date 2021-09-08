9c6fc134d03d5a174a61c782c4748382cb31c274
jtimonen <juho.timonen@iki.fi>
2021-09-08 03:39:30 +0300

    debug

--- latent/experiment.R
+++ latent/experiment.R
@@ -13,66 +13,71 @@ library(ggpubr)
 rstan_options(javascript = FALSE)
 rstan_options(auto_write = TRUE)
 
-# Run an experiment
-run_experiment <- function(n_per_N = 10, N = 6, model_idx = 1, chains = 1) {
-  scale_bf <- 4 / 3
-  NUM_BF <- c(20, 40, 60, 80, 100)
+# Settings
+n_per_N <- 10
+N <- 6
+model_idx <- 2
+chains <- 4
+scale_bf <- 3 / 2
+NUM_BF <- c(20, 40, 60)
+control <- list(adapt_delta = 0.95)
 
-  # Simulate data using lgpr
-  sd <- simulate_data(
-    N = N, t_data = seq(1, 5, length.out = n_per_N),
-    relevances = c(0, 1, 1),
-    covariates = c(2),
-    n_categs = c(3),
-    lengthscales = c(1.5, 1.0, 0.75), t_jitter = 0.2
-  )
-  dat <- sd@data
-  normalize_var <- function(x) (x - mean(x)) / stats::sd(x)
-  dat$y <- normalize_var(dat$y)
+# Simulate data using lgpr
+sd <- simulate_data(
+  N = N, t_data = seq(1, 5, length.out = n_per_N),
+  relevances = c(0, 1, 1),
+  covariates = c(2),
+  n_categs = c(3),
+  lengthscales = c(1.5, 1.0, 0.75), t_jitter = 0.2
+)
+dat <- sd@data
+normalize_var <- function(x) (x - mean(x)) / stats::sd(x)
+dat$y <- normalize_var(dat$y)
 
-  # Create model using lgpr
-  if (model_idx == 1) {
-    form <- y ~ age
-  } else if (model_idx == 2) {
-    form <- y ~ age + age | z
-  } else if (model_idx == 3) {
-    form <- y ~ age + age | z + id
-  } else {
-    form <- y ~ age + age | z + age | id
-  }
-  prior <- list(ell = igam(4, 4))
-  model <- create_model(form, dat, prior = prior, sample_f = TRUE)
-
-  # Approximate fits
-  NUM_CONF <- length(NUM_BF)
-  AFITS <- list()
-  for (i in seq_len(NUM_CONF)) {
-    cat("\n================================================================\n")
-    cat("i=", i, "\n", sep = "")
-    res <- sample_approx(model, NUM_BF[i], scale_bf,
-      chains = chains,
-      refresh = 500
-    )
-    AFITS[[i]] <- res$fit
-  }
-
-  # Exact fit
-  N <- model@stan_input$num_obs
-  cat("N=", N, "\n", sep = "")
-  if (N <= 200) {
-    sm_exact <- stan_model("stan/lgp_latent.stan")
-    fit_exact <- sampling(sm_exact,
-      data = model@stan_input, chains = chains,
-      pars = "eta", include = FALSE, refresh = 500
-    )
-  } else {
-    fit_exact <- NULL
-  }
-
-  # Return results
-  names(AFITS) <- NUM_BF
-  list(approx_fits = AFITS, exact_fit = fit_exact)
+# Create model using lgpr
+if (model_idx == 1) {
+  form <- y ~ age
+} else if (model_idx == 2) {
+  form <- y ~ age + age | z
+} else if (model_idx == 3) {
+  form <- y ~ age + age | z + id
+} else {
+  form <- y ~ age + age | z + age | id
 }
+prior <- list(ell = igam(4, 4))
+model <- create_model(form, dat, prior = prior, sample_f = TRUE)
+
+# Approximate fits
+NUM_CONF <- length(NUM_BF)
+AFITS <- list()
+for (i in seq_len(NUM_CONF)) {
+  cat("\n================================================================\n")
+  cat("i=", i, "\n", sep = "")
+  res <- sample_approx(model, NUM_BF[i], scale_bf,
+    chains = chains,
+    refresh = 500,
+    control = control
+  )
+  AFITS[[i]] <- res$fit
+}
+
+# Exact fit
+N <- model@stan_input$num_obs
+cat("N=", N, "\n", sep = "")
+if (N <= 200) {
+  sm_exact <- stan_model("stan/lgp_latent.stan")
+  fit_exact <- sampling(sm_exact,
+    data = model@stan_input, chains = chains,
+    pars = "eta", include = FALSE, refresh = 500,
+    control = control
+  )
+} else {
+  fit_exact <- NULL
+}
+
+# Collect results
+names(AFITS) <- NUM_BF
+res <- list(approx_fits = AFITS, exact_fit = fit_exact)
 
 # Get experiment results
 parse_results <- function(res) {
@@ -85,6 +90,9 @@ parse_results <- function(res) {
   get_pars <- function(x) {
     p <- extract(x, pars = c("alpha", "ell", "sigma"))
     return(cbind(p$alpha, p$ell, p$sigma))
+  }
+  get_ndiv <- function(x) {
+    sum(rstan::get_divergent_iterations(x))
   }
   nams <- c(names(res$approx_fits), "exact")
   ALL_FITS <- c(res$approx_fits, list(res$exact_fit))
@@ -101,9 +109,9 @@ parse_results <- function(res) {
     t_sds = sapply(ALL_FITS, t_sd),
     draws = draws,
     p_means = p_means,
-    p_sds = p_sds
+    p_sds = p_sds,
+    num_div = sapply(ALL_FITS, get_ndiv)
   )
 }
 
-res <- run_experiment(model_idx = 2, chains = 4)
 pres <- parse_results(res)
--- latent/stan/lgp_latent.stan
+++ latent/stan/lgp_latent.stan
@@ -20,38 +20,8 @@ functions{
 }
 
 data {
-
-  // Dimensions
-  int<lower=0> num_obs;         // number of observations
-  int<lower=0> num_cov_cont;    // number of continuous covariates
-  int<lower=0> num_cov_cat;     // number of categorical covariates
-  int<lower=1> num_comps;       // number of additive components
-  int<lower=0> num_ell;         // number of lengthscale parameters
-  int<lower=0> components[num_comps, 9];
-  int<lower=0> x_cat_num_levels[num_cov_cat];
-  real delta; // jitter to ensure pos. def. kernel matrices
-
-  // Covariates and response variable
-  vector[num_obs] x_cont[num_cov_cont];
-  int x_cat[num_cov_cat, num_obs];
-
-  // Priors: types and hyperparameters
-  int<lower=0> prior_alpha[num_comps, 2];  // {prior_type, transform}
-  int<lower=0> prior_ell[num_ell, 2];      // {prior_type, transform}
-  real hyper_alpha[num_comps, 3];
-  real hyper_ell[num_ell, 3];
-  
-  // Inputs specific to latent model
-  int<lower=1,upper=5> obs_model; // 1-5: Gaussian, Poisson, NB, Bin, BB
-  int<lower=0> y_int[obs_model>1, num_obs]; // response variable (int)
-  real y_real[obs_model==1, num_obs]; // response variable (real)
-  int<lower=1> y_num_trials[obs_model>3, num_obs]; // for Bin or BB model
-  int<lower=0> prior_sigma[obs_model==1, 2];
-  int<lower=0> prior_phi[obs_model==3, 2];
-  real hyper_sigma[obs_model==1, 3];
-  real hyper_phi[obs_model==3, 3];
-  real hyper_gamma[obs_model==5, 2];
-  vector[num_obs] c_hat; // GP mean vector
+#include data.stan
+  real<lower=0> delta;
 }
 
 transformed data{
@@ -64,11 +34,7 @@ transformed data{
 }
 
 parameters {
-  real<lower=1e-12> alpha[num_comps]; // component magnitudes
-  real<lower=1e-12> ell[num_ell]; // lengthscales
-  real<lower=1e-12> sigma[obs_model==1];
-  real<lower=1e-12> phi[obs_model==3];
-  real<lower=1e-12, upper=1-1e-12> gamma[obs_model==5];
+#include parameters.stan
   vector[num_obs] eta[num_comps]; // isotropic versions of func components
 }
 
@@ -86,49 +52,7 @@ transformed parameters {
 }
 
 model {
-
   vector[num_obs] f_sum = STAN_vectorsum(f_latent, num_obs) + c_hat;
   for(j in 1:num_comps){ target += normal_lpdf(eta[j] | 0, 1);}
-  
-  // Parameter priors
-  for(j in 1:num_comps){
-    target += STAN_log_prior(alpha[j], prior_alpha[j], hyper_alpha[j]);
-  }
-  for(j in 1:num_ell){
-    target += STAN_log_prior(ell[j], prior_ell[j], hyper_ell[j]);
-  }
-  if(obs_model==1){
-    target += STAN_log_prior(sigma[1], prior_sigma[1], hyper_sigma[1]);
-  }else if(obs_model==3){
-    target += STAN_log_prior(phi[1], prior_phi[1], hyper_phi[1]);
-  }else if(obs_model==5){
-    target += beta_lpdf(gamma[1] | hyper_gamma[1][2], hyper_gamma[1][2]);
-  }
-
-  // Likelihood
-  if(obs_model==1) {
-    // 1. Gaussian
-    real MU[num_obs] = to_array_1d(f_sum); // means
-    target += normal_lpdf(y_real[1] | MU, sigma[1]);
-  }else if(obs_model==2){
-    // 2. Poisson
-    real LOG_MU[num_obs] = to_array_1d(f_sum); // means (log-scale)
-    target += poisson_log_lpmf(y_int[1] | LOG_MU);
-  }else if(obs_model==3){
-    // 3. Negative binomial
-    real LOG_MU[num_obs] = to_array_1d(f_sum); // means (log-scale)
-    real PHI[num_obs] = to_array_1d(rep_vector(phi[1], num_obs)); // dispersion
-    target += neg_binomial_2_log_lpmf(y_int[1] | LOG_MU, PHI);
-  }else if(obs_model==4){
-    // 4. Binomial
-    real LOGIT_P[num_obs] = to_array_1d(f_sum); // p success (logit-scale)
-    target += binomial_logit_lpmf(y_int[1] | y_num_trials[1], LOGIT_P);
-  }else if(obs_model==5){
-    // 5. Beta-binomial
-    real tgam = inv(gamma[1]) - 1.0;
-    vector[num_obs] P = inv_logit(f_sum); // p success
-    real aa[num_obs] = to_array_1d(P * tgam);
-    real bb[num_obs] = to_array_1d((1.0 - P) * tgam);
-    target += beta_binomial_lpmf(y_int[1] | y_num_trials[1], aa, bb);
-  }
+#include model.stan
 }
