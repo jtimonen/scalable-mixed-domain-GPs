b0d057ba3f59ff0d5a1f1c5d82c786abf83f18c1
timonej3 <juho.timonen@aalto.fi>
2021-10-10 04:42:03 +0300

    add slurm job

---
+++ experiment_01/job.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH -p batch
+#SBATCH -t 01-12:00:00
+#SBATCH -n 1
+#SBATCH --mem=4000
+#SBATCH -o out/repl-%A.out
+module load r/3.6.1-python3
+srun Rscript --vanilla main_01.R $SLURM_ARRAY_TASK_ID
+
