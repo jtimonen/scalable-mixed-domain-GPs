79136894202b1ab844f3cb21f123ccefd2a1b571
timonej3 <juho.timonen@aalto.fi>
2021-10-10 22:12:22 +0300

    run main_01.sh with --array=1-30

--- experiment_01/main_01.sh
+++ experiment_01/main_01.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 02-00:00:00
+#SBATCH -t 01-12:00:00
 #SBATCH -n 1
-#SBATCH --array=1-100
-#SBATCH --mem=4000
+#SBATCH --array=1-30
+#SBATCH --mem=3000
 #SBATCH -o out/repl-%a.out
 module load r/4.0.3-python3
 srun Rscript --vanilla main_01.R $SLURM_ARRAY_TASK_ID
