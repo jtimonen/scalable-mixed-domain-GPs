416c416092911b4778b3cdbb3332f67eca350b44
timonej3 <juho.timonen@aalto.fi>
2021-10-12 10:17:49 +0300

    edit slurm script

--- simstudy_twocomp/simstudy_twocomp.sh
+++ simstudy_twocomp/simstudy_twocomp.sh
@@ -1,9 +1,10 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 01-12:00:00
+#SBATCH -t 00-08:00:00
 #SBATCH -n 1
 #SBATCH --array=1-30
-#SBATCH --mem=3000
+#SBATCH --mem=2700
+#SBATCH --constraint="skl|csl"
 #SBATCH -o out/repl-%a.out
 module load r/4.0.3-python3
 srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID
