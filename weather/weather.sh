5a8c88485401313418087c6f6b4299ff24a05ab0
timonej3 <juho.timonen@aalto.fi>
2021-10-13 03:50:15 +0300

    edit weather slurm script

--- weather/weather.sh
+++ weather/weather.sh
@@ -1,10 +1,9 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 02-00:00:00
+#SBATCH -t 02-12:00:00
 #SBATCH --cpus-per-task=4
-#SBATCH --array=8,12,16,20,24
-#SBATCH --mem=4000
-#SBATCH --constraint="skl"
+#SBATCH --array=24,32
+#SBATCH --mem=3000
 #SBATCH -o weather-%a.out
 module load r/4.0.3-python3
 srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID
