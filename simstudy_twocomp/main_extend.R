ee744e7d5dd7cadf974e6c02918170f27fdec907
timonej3 <juho.timonen@aalto.fi>
2021-10-15 04:15:57 +0300

    extend simstudy

---
+++ simstudy_twocomp/main_extend.R
@@ -0,0 +1,90 @@
+#!/usr/bin/env Rscript
+
+# Main R script for simstudy_twocomp
+args <- commandArgs(trailingOnly = TRUE)
+if (length(args) == 0) {
+  replication_idx <- 0
+} else {
+  replication_idx <- as.numeric(args[1])
+}
+cat("Currently in", getwd(), "\n")
+
+# Startup
+source(normalizePath(file.path("..", "common.R")))
+outdir <- startup(replication_idx)
+
+# Settings
+confs <- list()
+j <- 0
+SCALES <- c(1.5)#, 2.5, 4)
+NBFS <- c(4, 8, 16, 32, 64)
+for (scale_bf in SCALES) {
+  for (num_bf in NBFS) {
+    j <- j + 1
+    confs[[j]] <- create_configuration(num_bf, scale_bf)
+  }
+}
+
+# Global setup
+model_formula <- y ~ age + age | z
+N_test <- 150
+chains <- 4
+iter <- 2000
+refresh <- iter
+
+N_TRAIN <- c(300, 500, 1000, 2000) # , 150, 180, 210)
+for (N_train in N_TRAIN) {
+
+  # Generate data
+  sd <- simulate_data(N_train, N_test, 0.5)
+  train_dat <- sd$train_dat
+  test_dat <- sd$test_dat
+
+  # Create and fit exact model using lgpr
+  #exact_model <- lgpr::create_model(model_formula, train_dat)
+  #efit <- lgpr::sample_model(exact_model,
+  #  chains = chains, refresh = refresh,
+  #  iter = iter
+  #)
+
+  # Fit approximate model with different configurations
+  afits <- sample_approx(exact_model, confs,
+    refresh = refresh,
+    chains = chains,
+    adapt_delta = 0.95,
+    iter_warmup = iter / 2,
+    iter_sampling = iter / 2,
+    show_messages = FALSE
+  )
+
+  # Summarize results
+  fits <- afits #c(afits, list(efit))
+  #names(fits)[length(fits)] <- "exact"
+  sumr <- summarize_results(fits)
+
+  # Predict
+  #preds_train <- compute_predictions(fits, train_dat)
+  #preds_test <- compute_predictions(fits, test_dat)
+  #y_star <- test_dat[["y"]]
+
+  # Compute metrics
+  #em_test <- compute_metrics(fits, preds_test, y_star)
+  #rtables_test <- format_results(em_test, SCALES, NBFS)
+  #em_train <- compute_metrics(fits, preds_train, train_dat$y)
+  #rtables_train <- format_results(em_train, SCALES, NBFS)
+  #print(rtables_train)
+  #print(rtables_test)
+
+  # Save results to RDS file
+  all_results <- list(
+    train_dat = train_dat,
+    test_dat = test_dat,
+    summary = sumr,
+    #rtables_train = rtables_train,
+    #rtables_test = rtables_test,
+    sess_info = sessionInfo()
+  )
+  fn <- file.path(outdir, paste0("res_", N_train, ".rds"))
+  cat(" * Saving to", fn, "\n")
+  saveRDS(all_results, file = fn)
+}
--- simstudy_twocomp/simstudy_twocomp.sh
+++ simstudy_twocomp/simstudy_twocomp.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 00-08:00:00
+#SBATCH -t 00-10:00:00
 #SBATCH -n 1
 #SBATCH --array=1-30
-#SBATCH --mem=2700
-#SBATCH --constraint="skl|csl"
+#SBATCH --mem=3500
 #SBATCH -o out/repl-%a.out
 module load r/4.0.3-python3
-srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID
+srun Rscript --vanilla main_extend.R $SLURM_ARRAY_TASK_ID
 
