0b28adcb90348fa3180986790a160219fa649bc1
timonej3 <juho.timonen@aalto.fi>
2024-05-28 13:57:24 +0300

    try to run on triton

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 #SBATCH -p batch
-#SBATCH -t 15:00:00
+#SBATCH -t 03:00:00
 #SBATCH -n 1
 #SBATCH --mem=3000
-#SBATCH --array=1-120
+#SBATCH --array=1-1
 #SBATCH -o out/run-%a.out
 
 export R_LIBS=/scratch/work/timonej3/Rlibs-4.1.1
-module load r/4.1.1-python3
-srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID 1
-#srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID 2
+module load r/4.3.0
+srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID 20 0.25 1
