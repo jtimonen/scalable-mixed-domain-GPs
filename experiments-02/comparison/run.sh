c95a830c8c59bbc2c35d73133e851672bd4260fb
timonej3 <juho.timonen@aalto.fi>
2024-06-15 06:52:28 +0300

    edit slurm script

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -3,9 +3,7 @@
 #SBATCH -t 03:00:00
 #SBATCH -n 1
 #SBATCH --mem=3000
-#SBATCH --array=1-1
+#SBATCH --array=1-3
 #SBATCH -o out/run-%a.out
 
-export R_LIBS=/scratch/work/timonej3/Rlibs-4.1.1
-module load r/4.3.0
 srun Rscript --vanilla main.R $SLURM_ARRAY_TASK_ID 20 0.25 1
