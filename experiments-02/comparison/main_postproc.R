f4d56c80beb32e21263f4fe312d4eb01119eb443
Juho Timonen <juho.timonen@aalto.fi>
2024-06-19 13:19:45 +0300

    improve scalar result collection

--- experiments-02/comparison/main_postproc.R
+++ experiments-02/comparison/main_postproc.R
@@ -4,6 +4,7 @@ library(tidyverse)
 ls <- dir("res")
 ls <- ls[grepl(pattern = "res-", ls)]
 
+# Get scalar results
 get_scalar_res <- function(dir) {
   rdir <- file.path("res", dir)
   files <- dir(rdir)
@@ -15,14 +16,30 @@ get_scalar_res <- function(dir) {
   tibble::as_tibble(out)
 }
 
+# Scalar results to data frame
 res_to_df <- function(fn) {
   r <- readRDS(fn)
   x <- unlist(r[c("N_indiv", "snr")])
   x_diag <- r[["diag"]]
-  x <- c(x, x_diag)
+  num_terms <- length(r$term_names)
+  x <- c(x, x_diag, num_terms)
+  names(x)[length(x)] <- "num_terms"
   x <- data.frame(t(c(basename(fn), x)))
   colnames(x)[1] <- "file"
   x
+}
+
+
+# Get scalar results
+get_path_res <- function(dir) {
+  rdir <- file.path("res", dir)
+  files <- dir(rdir)
+  out <- NULL
+  for (j in seq_len(length(files))) {
+    fn <- file.path(rdir, files[j])
+    out <- rbind(out, res_to_df(fn))
+  }
+  tibble::as_tibble(out)
 }
 
 # Scalar results
