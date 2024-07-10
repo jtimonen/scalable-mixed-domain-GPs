85b9615a7b2ecf963a6e757a47be2a1fcbde50cc
timonej3 <juho.timonen@aalto.fi>
2024-07-10 07:47:03 +0300

    edit cluster install again

---
+++ experiments-02/comparison/install.R
@@ -0,0 +1,7 @@
+message("Hello")
+repos <- 'http://cran.us.r-project.org'
+install.packages("lgpr", repos = repos)
+library(lgpr)
+
+message("done")
+
--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -3,9 +3,9 @@
 #SBATCH -t 08:00:00
 #SBATCH -n 1
 #SBATCH --mem=3000
-#SBATCH --array=1-100
+#SBATCH --array=1-2
 #SBATCH -o out/run-%a.out
 unset LC_ALL
-export CXX=/home/timonej3/.conda/envs/rstan-env/bin/g++
-srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 40 0.25 1
+export CXX=/home/timonej3/.conda/envs/stan-env/bin/g++
+srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 40 0.5 1
 
