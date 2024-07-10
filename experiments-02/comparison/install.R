e7185b7802d3005d89136aad56fdc245c4b7b878
timonej3 <juho.timonen@aalto.fi>
2024-07-10 08:29:41 +0300

    install of cmdstanr not working

--- experiments-02/comparison/install.R
+++ experiments-02/comparison/install.R
@@ -1,8 +1,11 @@
 message("Hello")
 repos <- 'http://cran.us.r-project.org'
 #install.packages("lgpr", repos = repos)
-devtools::install_github("jtimonen/lgpr2")
-library(lgpr)
+#devtools::install_github("jtimonen/lgpr2", build_vignettes=FALSE, dependencies=TRUE)
+#library(lgpr2)
+
+install.packages("cmdstanr", repos = repos) #c('https://stan-dev.r-universe.dev', getOption("repos")))
+
 
 message("done")
 
