74d22d7199e9dee387fddc443d752eb62987a838
timonej3 <juho.timonen@aalto.fi>
2024-07-23 14:57:31 +0300

    too ez

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -7,5 +7,5 @@
 #SBATCH -o out/run-%a.out
 unset LC_ALL
 export CXX=/home/timonej3/.conda/envs/stan-env/bin/g++
-srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 100 0.25 1
+srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 100 0.1 1
 
