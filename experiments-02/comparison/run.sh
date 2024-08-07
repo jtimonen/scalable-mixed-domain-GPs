338bfe915d8563c6d48ee4845e55d176eb3e90e2
timonej3 <juho.timonen@aalto.fi>
2024-08-07 11:49:42 +0300

    problem on cluster

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -4,8 +4,8 @@
 #SBATCH -n 1
 #SBATCH --mem=6000
 #SBATCH --array=1-100
-#SBATCH -o out/run-%a.out
+#SBATCH -o out/run-02-%a.out
 unset LC_ALL
 export CXX=/home/timonej3/.conda/envs/stan-env/bin/g++
-srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 100 0.1 1
+srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 100 0.2 1
 
