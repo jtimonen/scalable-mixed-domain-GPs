b0fc366667ce0507ae4c207106d2635ea5f43d97
timonej3 <juho.timonen@aalto.fi>
2024-08-20 22:00:08 +0300

    more experiments

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -2,9 +2,9 @@
 #SBATCH -p batch
 #SBATCH -t 08:00:00
 #SBATCH -n 1
-#SBATCH --mem=6000
-#SBATCH --array=1-10
-#SBATCH -o out/run-02-%a.out
+#SBATCH --mem=12000
+#SBATCH --array=51-100
+#SBATCH -o out/run-02b-%a.out
 unset LC_ALL
 export CXX=/home/timonej3/.conda/envs/stan-env/bin/g++
 srun Rscript --vanilla --verbose main.R $SLURM_ARRAY_TASK_ID 100 0.2 1
