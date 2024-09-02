b3e711c34660065648e4e0084e9ed454990f4b91
timonej3 <juho.timonen@aalto.fi>
2024-09-02 06:16:17 +0300

    edit slurm script

--- experiments-02/comparison/run.sh
+++ experiments-02/comparison/run.sh
@@ -3,7 +3,7 @@
 #SBATCH -t 08:00:00
 #SBATCH -n 1
 #SBATCH --mem=12000
-#SBATCH --array=1-100
+#SBATCH --array=1-150
 #SBATCH -o out/run-%a.out
 unset LC_ALL
 export CXX=/home/timonej3/.conda/envs/stan-env/bin/g++
