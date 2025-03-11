532b35c61b14dcc95fcae7e7a5098057dfd6cc3c
Juho Timonen <juho.timonen@aalto.fi>
2025-03-11 02:37:28 +0200

    improve large data scaling experiment

--- experiments-02/nonseparable/main_large.R
+++ experiments-02/nonseparable/main_large.R
@@ -9,30 +9,72 @@ source("functions_e2.R")
 sigma_true <- 0.1
 
 set.seed(123)
-X <- simulate_input_vary(100, 10)
-df <- simulate_fast(X)
-df <- simulate_obs(df, sigma_true)
-df <- create_dummy_x(df)
-plt <- ggplot(df, aes(x = x, y = f, color = z)) +
-  geom_line() +
-  geom_point(mapping = aes(x = x, y = y, color = z)) +
-  facet_grid(. ~ z)
-a <- split_train_test(df, test_categ = c(2, 3))
-df_train <- a$train
-df_test <- a$test
-df <- a$full
-
-# Fit
 m2 <- lgpr2:::LonModel$new(formula = y ~ gp(x, z) + gp(x))
-t_start <- now()
-f2 <- m2$fit(
-  data = df_train, chains = 1, iter_sampling = 30, iter_warmup = 30,
-  refresh = 1
-)
-t_end <- now()
-t_dur <- t_end - t_start
 
-r2 <- f2$predict(df)$function_draws()$get_output()
-r2 <- FunctionDraws$new(df, r2, "Kernel 1")
 
-p2 <- plot_fit(r2, df, df_train, df_test, FALSE, TRUE)
+run_exp <- function(N) {
+  X <- simulate_input_vary(N, 10)
+  df <- simulate_fast(X)
+  df <- simulate_obs(df, sigma_true)
+  df <- create_dummy_x(df)
+  plt <- ggplot(df, aes(x = x, y = f, color = z)) +
+    geom_line() +
+    geom_point(mapping = aes(x = x, y = y, color = z)) +
+    facet_grid(. ~ z)
+  a <- split_train_test(df, test_categ = c(2, 3), alt = FALSE)
+  df_train <- a$train
+  df_test <- a$test
+  df <- a$full
+
+  # Fit
+  t_start <- Sys.time()
+  f2 <- m2$fit(
+    data = df_train, chains = 1, iter_sampling = 4, iter_warmup = 4,
+    refresh = 1
+  )
+  t_end <- Sys.time()
+  t_dur <- as.numeric(t_end - t_start, units = "secs")
+  t_stan <- f2$get_stan_fit()$time()$total
+
+  # r2 <- f2$predict(df)$function_draws()$get_output()
+  # r2 <- FunctionDraws$new(df, r2, "Kernel 1")
+
+  # plot <- plot_fit(r2, df, df_train, df_test, FALSE, TRUE)
+  size <- nrow(df_train)
+  message(size)
+  return(lst(t_dur, t_stan, size))
+}
+res <- NULL
+sizes <- 2 * 10^c(1, 2, 3, 4, 5)
+j <- 0
+for (s in sizes) {
+  j <- j + 1
+  res[[j]] <- run_exp(s)
+}
+get_res <- function(x) {
+  a <- c(x$size, x$t_dur, x$t_stan)
+  df <- data.frame(a)
+  df <- t(df)
+  colnames(df) <- c("N", "t_full", "t_mcmc")
+  t(df)
+}
+out <- data.frame(t(sapply(res, get_res)))
+colnames(out) <- c("N", "t_full", "t_mcmc")
+out$prop_overhead <- (out$t_full - out$t_mcmc) / (out$t_full)
+
+plt_a <- out %>%
+  ggplot(aes(x = N, y = t_mcmc)) +
+  geom_line() +
+  geom_point() +
+  scale_y_log10() +
+  scale_x_log10() +
+  ylab("Time (seconds)")
+
+plt_b <- out %>%
+  ggplot(aes(x = N, y = prop_overhead)) +
+  geom_line() +
+  geom_point() +
+  scale_x_log10() +
+  ylab("Proportion of overhead time")
+
+plt <- ggarrange(plt_a, plt_b, labels = c("a)", "b)"), nrow = 2)
